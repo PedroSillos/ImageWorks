58d5995e9292a819f74de41ec1fac8cd0c6aa8a8
Pedro Sillos <pedro.sillos@gmail.com>
2020-09-02 11:36:58 -0300

    Added image file opening and pixel color histogram

--- Image_processing.py
+++ Image_processing.py
@@ -1,17 +1,25 @@
 import cv2
-import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
 
 try:
     #busca a imagem do endereço e armazena como matriz
-    a_imagem_grande = cv2.imread('C:/Users/psillosx/OneDrive - Intel Corporation/Desktop/a_imagem.jpg')
+    a_imagem_grande = cv2.imread('C:/Users/psillosx/OneDrive - Intel Corporation/Desktop/ImageWorks/a_imagem.jpg')
     
     #corta a imagem para 360x360:
     a_imagem_ideal = a_imagem_grande[160:520,400:760]
 
-    #dimensoes da imagem
-    print('Numero de linhas da imagem: '+str(a_imagem_ideal.shape[0]))
-    print('Numero de colunas da imagem: '+str(a_imagem_ideal.shape[1]))
-    print('Espectros de cores por pixel: '+str(a_imagem_ideal.shape[2]))
+    #conta as ocorrencias das cores da imagem e ordena por cor, de 0 a 255
+    #numero de ocorrencias de uma cor: (ex. 123)
+    #n = 194
+    #print('Valor da cor',n,':',contagem_de_cores[n][1])
     
+    x = list()
+    for linha in a_imagem_ideal:
+        for pixel in linha:
+            x.append(pixel[0])
+    plt.hist(x, bins=255)
+    plt.show()
+
 except:
     print('Nao foi possivel encontrar a imagem')
