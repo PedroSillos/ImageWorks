1144edb667594412d231360e87d0ea77b78982ef
Pedro Sillos <pedro.sillos@gmail.com>
2020-09-03 09:38:19 -0300

    Almost finished, just need to wrap everything up

--- Image_processing.py
+++ Image_processing.py
@@ -1,24 +1,40 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image
+
+def abrir_arquivo():
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename()
+    return file_path
 
 try:
-    #busca a imagem do endereço e armazena como matriz
-    a_imagem_grande = cv2.imread('C:/Users/psillosx/OneDrive - Intel Corporation/Desktop/ImageWorks/a_imagem.jpg')
-    
-    #corta a imagem para 360x360:
-    a_imagem_ideal = a_imagem_grande[160:520,400:760]
+    #abre arquivo e retorna seu endereço
+    file_path = abrir_arquivo()
 
-    #conta as ocorrencias das cores da imagem e ordena por cor, de 0 a 255
-    #numero de ocorrencias de uma cor: (ex. 123)
-    #n = 194
-    #print('Valor da cor',n,':',contagem_de_cores[n][1])
+    #busca a imagem do endereço e armazena como matriz
+    a_imagem_original = cv2.imread(file_path)
+
+    #corta a imagem para 360x360:
+    a_imagem_cortada = a_imagem_original[160:520,400:760]
+
+    #converte a matriz multidimensional de volta para imagem e exibe
+    cv2.imshow('Imagem cortada', a_imagem_cortada)
     
-    x = list()
-    for linha in a_imagem_ideal:
+    #a imagem é uma matriz de 3 dimensoes, aqui pegamos a cor de cada pixel e colocamos numa lista
+    cores_dos_pixels = list()
+    for linha in a_imagem_cortada:
         for pixel in linha:
-            x.append(pixel[0])
-    plt.hist(x, bins=255)
+            cor = pixel[0] #cada pixel tem 3 ints (rgb), aqui só queremos 1
+            cores_dos_pixels.append(cor)
+
+    print('\nEssa imagem tem:',len(cores_dos_pixels),'pixels')
+
+    espectro_de_cores = 255
+    plt.hist(cores_dos_pixels, bins=espectro_de_cores)
     plt.show()
 
 except:
---
+++ window_managment.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+janela = Tk(className='Editor de imagens')
+
+espaco_de_cima = Frame(janela, width=1080, height=540)
+espaco_de_cima.pack()
+
+carregar_foto = Button()
+
+imagem_original = PhotoImage(file=)
+
+janela.mainloop()
